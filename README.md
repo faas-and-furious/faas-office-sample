d63572d9a4d69e291e428426b47cf21958425b06
Alex Ellis <alexellis2@gmail.com>
2017-08-23 11:17:56 +0000

    Merge changes from Johnny
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- README.md
+++ README.md
@@ -6,12 +6,22 @@ This is a quick demo of what can be done easily with [OpenFaaS](https://github.c
 
 ## Create a PowerPoint doc with FaaS
 
-This is a quick way to deploy, if you already have FaaS and the CLI setup.
+This takes a JSON input as defined in "sample.json" with a title, text and image URL then creates a presentation file.
+
+```json
+{
+    "image_url": "https://pbs.twimg.com/profile_images/894625939652579328/Cmbsq0OP_400x400.jpg",
+    "text": "OpenFaaS",
+    "title": "Serverless on your servers.. yas"
+}
+```
+
+Here is a quick way to deploy, if you already have FaaS and the CLI setup:
 
 ```
 $ faas-cli -action build -f ./stack.yml 
 $ faas-cli -action deploy -f ./stack.yml 
-$ curl -d "FaaS community" http://localhost:8080/function/on-deck > faas.pptx
+$ curl --data-binary @sample.json http://localhost:8080/function/on-deck > faas.pptx
 ```
 
 ![](https://user-images.githubusercontent.com/6358735/29585241-546cd994-877e-11e7-88dc-127d65fdf3f7.png)
@@ -19,3 +29,6 @@ $ curl -d "FaaS community" http://localhost:8080/function/on-deck > faas.pptx
 ## Watch for more
 
 Keep an eye out for how to use this with a datasource like the top trending Tweets, Hacker News stories or news headlines.
+
+* [Other libraries for serverless documents](https://gist.github.com/alexellis/33f946ea83607ed074df7e282c65be6a)
+
--- on-deck/handler.py
+++ on-deck/handler.py
@@ -1,17 +1,60 @@
-from pptx import Presentation
+import json
 import sys
+import os.path
 
-def handle(req):
+from pptx import Presentation
+from pptx.util import Inches
+from six.moves import urllib
+
+# {
+#     "image_url": "https://pbs.twimg.com/profile_images/894625939652579328/Cmbsq0OP_400x400.jpg",
+#     "text": "OpenFaaS",
+#     "title": "Serverless on your servers.. yas"
+# }
+
+def maybe_download_url(url, dest_directory):
+    if not os.path.exists(dest_directory):
+        os.makedirs(dest_directory)
+
+    filename = url.split('/')[-1]
+    filepath = os.path.join(dest_directory, filename)
+    if not os.path.exists(filepath):
+        filepathResult, _ = urllib.request.urlretrieve(url, filepath)
+
+        # statinfo = os.stat(filepath)
+
+    return filepath
+
+def make_prezo(req):
     prs = Presentation()
     title_slide_layout = prs.slide_layouts[0]
+
     slide = prs.slides.add_slide(title_slide_layout)
     title = slide.shapes.title
+
     subtitle = slide.placeholders[1]
+    title.text = req["title"]
+    subtitle.text = req["text"]
 
-    title.text = "Hello " + req
-    subtitle.text = "python-pptx was here!"
+    top = Inches(1)
+    left = Inches(2)
+    height = Inches(1.5)
 
+    img = maybe_download_url(req["image_url"], "/tmp")
+    pic = slide.shapes.add_picture(img, left, top, height=height)
+    return prs
+
+def output_prezo(prs):
     prs.save("out.pptx")
     myfile = open('out.pptx', 'r')
-    sys.stdout.write(myfile.read())
+    data = myfile.read()
     myfile.close()
+    sys.stdout.write(data)
+
+
+def handle(val):
+    req = json.loads(val)
+
+    prs = make_prezo(req)
+
+    output_prezo(prs)
